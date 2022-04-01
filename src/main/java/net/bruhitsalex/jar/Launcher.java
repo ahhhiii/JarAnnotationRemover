b3c15ac32fcff7d960b55e464dabdb0304dd62dd
alexh <alex.hammonnd003@gmail.com>
2022-04-02 00:20:29 +0100

    Initial commit

---
+++ src/main/java/net/bruhitsalex/jar/Launcher.java
@@ -0,0 +1,9 @@
+package net.bruhitsalex.jar;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/net/bruhitsalex/jar/Processor.java
@@ -0,0 +1,4 @@
+package net.bruhitsalex.jar;
+
+public class Processor {
+}
